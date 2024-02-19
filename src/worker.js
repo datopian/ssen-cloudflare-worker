2fd08248da27c3f6c93d0c4688c75b2676501e7e
anuveyatsu <anuar.ustayev@gmail.com>
2024-02-19 23:59:24 +0600

    Initial commit (by create-cloudflare CLI)
    Details:
      C3 = create-cloudflare@2.12.0
      project name = ssen-smart-meter
      package manager = npm@9.6.7
      wrangler = wrangler@3.28.3
      git = 2.39.3 (Apple Git-145)

---
+++ src/worker.js
@@ -0,0 +1,29 @@
+// src/worker.ts
+var worker_default = {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    const key = url.pathname.slice(1);
+    const headers = {
+      "content-type": "application/json; charset=UTF-8",
+      "Access-Control-Allow-Origin": "*"
+    };
+    if (request.method === "GET" || request.method === "HEAD") {
+      if (request.method == "HEAD") {
+        return new Response(void 0, { status: 400, headers });
+      }
+      const listing = await env.SSEN_BUCKET.list({ prefix: key });
+      listing.objects = listing.objects.map((e) => ({
+        ...e,
+        downloadLink: env.PUBLIC_BUCKET_URI + e.key
+      }));
+      return new Response(JSON.stringify(listing), { headers });
+    }
+    return new Response(`Unsupported method`, {
+      status: 400
+    });
+  }
+};
+export {
+  worker_default as default
+};
+//# sourceMappingURL=worker.js.map
